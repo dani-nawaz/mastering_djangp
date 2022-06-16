f7abca4f565a401e717b60057e960ae1e6ee49c5
Abdul Rehman Nawaz <daninawaz9@gmail.com>
2022-06-16 16:14:12 +0500

    Finish testing DRF with mocks

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -28,10 +28,10 @@ urlpatterns = [
     path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
 ]
 
-urlpatterns += [
-    path("auth/", include("rest_framework.urls")),
-    path("token-auth/", views.obtain_auth_token)
-]
+# urlpatterns += [
+#     path("auth/", include("rest_framework.urls")),
+#     path("token-auth/", views.obtain_auth_token)
+# ]
 urlpatterns = format_suffix_patterns(urlpatterns)
 
 urlpatterns += [
