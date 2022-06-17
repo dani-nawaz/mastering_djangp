19efbd16f0d94b72cf0cd973f16f3177403ec209
Abdul Rehman Nawaz <daninawaz9@gmail.com>
2022-06-17 19:51:41 +0500

    Finish filtering

--- blog/api/views.py
+++ blog/api/views.py
@@ -67,6 +67,77 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     throttle_classes = [ScopedRateThrottle]
     throttle_scope = "post_api"
 
+    # def get_queryset(self):
+    #     if self.request.user.is_anonymous:
+    #         # published only
+    #         return self.queryset.filter(published_at__lte=timezone.now())
+    #
+    #     if self.request.user.is_staff:
+    #         # allow all
+    #         return self.queryset
+    #
+    #     # filter for own or
+    #     return self.queryset.filter(
+    #         Q(published_at__lte=timezone.now()) | Q(author=self.request.user)
+    #     )
+    # def get_queryset(self):
+    #     # queryset has been set by applying user filtering rules
+    #
+    #     # fetch the period_name URL parameter from self.kwargs
+    #     time_period_name = self.kwargs.get("period_name")
+    #
+    #     if not time_period_name:
+    #         # no further filtering required
+    #         return queryset
+    #
+    #     if time_period_name == "new":
+    #         return queryset.filter(published_at__gte=timezone.now() - timedelta(hours=1))
+    #     elif time_period_name == "today":
+    #         return queryset.filter(
+    #             published_at__date=timezone.now().date(),
+    #         )
+    #     elif time_period_name == "week":
+    #         return queryset.filter(published_at__gte=timezone.now() - timedelta(days=7))
+    #     else:
+    #         raise Http404(
+    #             f"Time period {time_period_name} is not valid, should be "
+    #             f"'new', 'today' or 'week'"
+    #         )
+    # def get_queryset(self):
+    #     if self.request.user.is_anonymous:
+    #         # published only
+    #         queryset = self.queryset.filter(published_at__lte=timezone.now())
+    #
+    #     elif not self.request.user.is_staff:
+    #         # allow all
+    #         queryset = self.queryset
+    #     else:
+    #         queryset = self.queryset.filter(
+    #             Q(published_at__lte=timezone.now()) | Q(author=self.request.user)
+    #         )
+    #
+    #     time_period_name = self.kwargs.get("period_name")
+    #
+    #     if not time_period_name:
+    #         # no further filtering required
+    #         return queryset
+    #
+    #     if time_period_name == "new":
+    #         return queryset.filter(
+    #             published_at__gte=timezone.now() - timedelta(hours=1)
+    #         )
+    #     elif time_period_name == "today":
+    #         return queryset.filter(
+    #             published_at__date=timezone.now().date(),
+    #         )
+    #     elif time_period_name == "week":
+    #         return queryset.filter(published_at__gte=timezone.now() - timedelta(days=7))
+    #     else:
+    #         raise Http404(
+    #             f"Time period {time_period_name} is not valid, should be "
+    #             f"'new', 'today' or 'week'"
+    #         )
+
 
 class UserDetail(generics.RetrieveAPIView):
     lookup_field = "email"
