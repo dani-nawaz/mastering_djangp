5948f1185deaef01a3570793a939899ec0aab81c
Abdul Rehman Nawaz <daninawaz9@gmail.com>
2022-06-17 17:48:27 +0500

    Finish caching

--- blog/api/views.py
+++ blog/api/views.py
@@ -2,10 +2,58 @@ from rest_framework import generics, viewsets
 
 from blango_auth.models import User
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
-from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer, TagSerializer
-from blog.models import Post
+from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer, TagSerializer, CommentSerializer
+from blog.models import Post, Comment
 from blog.models import Tag
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers, vary_on_cookie
 
+from rest_framework.exceptions import PermissionDenied
+
+
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+
+
+# class PostViewSet(viewsets.ModelViewSet):
+#     # existing attributes omitted
+#
+#     @action(methods=["get"], detail=False, name="Posts by the logged in user")
+#     def mine(self, request):
+#         if request.user.is_anonymous:
+#             raise PermissionDenied("You must be logged in to see which Posts are yours")
+#         posts = self.get_queryset().filter(author=request.user)
+#         serializer = PostSerializer(posts, many=True, context={"request": request})
+#         return Response(serializer.data)
+
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers, vary_on_cookie
+
+
+# class PostViewSet(viewsets.ModelViewSet):
+#     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
+#     queryset = Post.objects.all()
+#
+#     def get_serializer_class(self):
+#         if self.action in ("list", "create"):
+#             return PostSerializer
+#         return PostDetailSerializer
+#
+# @method_decorator(cache_page(120))
+# def list(self, *args, **kwargs):
+    # return super(PostViewSet, self).list(*args, **kwargs)
+#     @method_decorator(cache_page(300))
+#     @method_decorator(vary_on_headers("Authorization"))
+#     @method_decorator(vary_on_cookie)
+#     @action(methods=["get"], detail=False, name="Posts by the logged in user")
+#     def mine(self, request):
+#         if request.user.is_anonymous:
+#             raise PermissionDenied("You must be logged in to see which Posts are yours")
+#         posts = self.get_queryset().filter(author=request.user)
+#         serializer = PostSerializer(posts, many=True, context={"request": request})
+#         return Response(serializer.data)
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
